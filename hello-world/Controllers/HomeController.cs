9771bbf72447417fee8f7454002652d047818ed1
Sam <sam@samspain.com>
2021-07-05 07:47:47 +0100

    Pass model into view

--- hello-world/Controllers/HomeController.cs
+++ hello-world/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using hello_world.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,15 +12,24 @@ namespace hello_world.Controllers
     [Route("Home/Index")]
     public class HomeController : Controller
     {
-        public string Index()
+        public ActionResult Index()
         {
-            return "Hello from home controller";
+            Book book = new Book()
+            {
+                Title = "Learning ASP",
+                Description = "Sam's attempt to learn this stuff",
+                Author = "Andrew Anders",
+                PublishDate = "July, 2021",
+                Price = 35,
+                Image = ""
+            };
+            return View(book);
         }
 
         [Route("about")]
-        public string About()
+        public ActionResult About()
         {
-            return "About us";
+            return View();
         }
 
         [Route("contact")]
---
+++ hello-world/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace hello_world.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public string PublishDate { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+    }
+}
